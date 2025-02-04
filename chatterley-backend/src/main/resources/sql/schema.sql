fc4067e890b6ccabaf3456674963c54543e6154c
ilya-kuznetsov <et.anchoret@gmail.com>
2025-02-05 01:12:02 +0300

    Postgresql schema and docker-compose configuration were added.

---
+++ chatterley-backend/src/main/resources/sql/schema.sql
@@ -0,0 +1,7 @@
+create table users (
+  id varchar(32) primary key,
+  nickname varchar(200) not null,
+  password_hash varchar(200) not null,
+  create_date bigint not null,
+  constraint uq_user_nickname unique (nickname)
+);
